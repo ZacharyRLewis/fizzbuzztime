a4f97703a534ca30406dbc7e9f6a533b373723a0
Zach Lewis <ZacharyRLewis@gmail.com>
2024-11-27 00:03:38 -0500

    Add timer component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PromptComponent } from 'prompt';
+import { TimerComponent } from 'timer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'prompt', component: PromptComponent},
+  {path: 'timer', component: TimerComponent}
+];
---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'timer',
+  imports: [],
+  templateUrl: './timer.component.html',
+  styleUrl: './timer.component.scss'
+})
+export class TimerComponent {
+
+}
